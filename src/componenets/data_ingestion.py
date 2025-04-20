2bacc2109238d919034fa7ace7f27da9a2b07f32
Anjali Sharma <anjalisharmarbt@gmail.com>
2025-04-20 11:23:45 +0530

    data transformation

--- src/componenets/data_ingestion.py
+++ src/componenets/data_ingestion.py
@@ -5,6 +5,8 @@ from src.logger import logging
  
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from src.componenets.data_transformation import DataTransformationConfig
+from src.componenets.data_transformation import DataTransformation
 from dataclasses import dataclass
  
 @dataclass #we will be directly able to define our class variable
@@ -44,5 +46,8 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
     
