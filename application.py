1f3b7a738d8b00407714b4e777cefbca316b777c
Anjali Sharma <anjalisharmarbt@gmail.com>
2025-04-21 22:32:50 +0530

    done

--- application.py
+++ application.py
@@ -26,8 +26,8 @@ def predict_datapoint():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+            reading_score=float(request.form.get('reading_score')),
+            writing_score=float(request.form.get('writing_score'))
 
         )
         pred_df=data.get_data_as_data_frame()
@@ -39,7 +39,9 @@ def predict_datapoint():
         results=predict_pipeline.predict(pred_df)
         print("after Prediction")
         
-        return render_template('home.html',results=results[0])
+        results_rounded = [round(result, 2) for result in results]
+
+        return render_template('home.html', results=results_rounded[0])
     
 
 if __name__=="__main__":
